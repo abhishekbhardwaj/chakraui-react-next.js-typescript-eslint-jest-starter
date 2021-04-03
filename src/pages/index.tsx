ecd7356e3c01e10f9aa77013430fc5f4fcd8a564
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2021-04-03 11:59:40 -0700

    SITE_NAME --> APP_NAME

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,13 @@ import { Flex } from '@chakra-ui/react'
 import MainHeading from '@src/components/main-heading/main-heading'
 
 const Home: NextPage = () => {
-  const { SITE_NAME } = process.env
+  const { APP_NAME } = process.env
 
   return (
     <Flex bg="gray.50" minHeight="100vh" alignItems="center" justifyContent="center">
       <Head>
         <meta charSet="utf-8" />
-        <title>{SITE_NAME}</title>
+        <title>{APP_NAME}</title>
       </Head>
       <MainHeading>Hello World.</MainHeading>
     </Flex>
