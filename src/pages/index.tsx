7233cc3396470c9ce9236873554e04744696fb82
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2022-09-06 01:37:51 -0700

    upgrades + lint-staged + husky.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import Head from 'next/head'
 import { NextPage } from 'next'
 import { Flex } from '@chakra-ui/react'
+import { publicRuntimeConfig } from '@src/config'
 
 import MainHeading from '@src/components/main-heading/main-heading'
 
 const Home: NextPage = () => {
-  const { APP_NAME } = process.env
-
   return (
     <Flex bg="gray.50" minHeight="100vh" alignItems="center" justifyContent="center">
       <Head>
         <meta charSet="utf-8" />
-        <title>{APP_NAME}</title>
+        <title>{publicRuntimeConfig.APP_NAME}</title>
       </Head>
       <MainHeading>Hello World.</MainHeading>
     </Flex>
